d1e3438f092f61de165a2283781fca3257baf4e9
Antonio Romano <aromanomacias@gmail.com>
2022-03-18 19:54:54 -0600

    adding mail connector

---
+++ back/server/models/mail.js
@@ -0,0 +1,18 @@
+'use strict';
+
+module.exports = function(Mail) {
+
+    Mail.SendEmail = function(emailData, callback) {
+        Mail.send({
+            to: emailData.to,
+            from: emailData.from,
+            subject: emailData.subject,
+            text: emailData.text,
+            html: emailData.html
+        }, (err, mailSent) => {
+            if(err) return callback(err);
+            return callback(null, 'sent');
+        })
+    }
+
+};
