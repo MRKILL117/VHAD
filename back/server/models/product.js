118442fba195f5c66993768c04291d6125784b23
Antonio Romano <aromanomacias@gmail.com>
2022-04-03 00:36:22 -0500

    Product CRUD

--- back/server/models/product.js
+++ back/server/models/product.js
@@ -2,4 +2,39 @@
 
 module.exports = function(Product) {
 
+    Product.CreateProduct = function(product, callback) {
+        Product.create(product, (err, newProduct) => {
+            if(err) return callback(err);
+
+            return callback(null, newProduct);
+        });
+    }
+
+    Product.GetProducts = function(callback) {
+        Product.find({where: {isDeleted: false}}, (err, products) => {
+            if(err) return callback(err);
+
+            return callback(null, products);
+        })
+    }
+
+    Product.prototype.UpdateProduct = function(product, callback) {
+        this.name = product.name;
+        this.description = product.description;
+        this.save((err, product) => {
+            if(err) return callback(err);
+
+            return callback(null, product);
+        });
+    }
+
+    Product.prototype.DeleteProduct = function(callback) {
+        this.isDeleted = true;
+        this.save((err, product) => {
+            if(err) return callback(err);
+
+            return callback(null, product);
+        });
+    }
+
 };
--- front/src/app/pages/products/products.component.ts
+++ front/src/app/pages/products/products.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { BsModalService } from 'ngx-bootstrap/modal';
+import { FormService } from 'src/app/services/form.service';
+import { HttpService } from 'src/app/services/http.service';
+import { ToastService } from 'src/app/services/toast.service';
 
 @Component({
   selector: 'app-products',
@@ -10,23 +13,80 @@ import { BsModalService } from 'ngx-bootstrap/modal';
 export class ProductsComponent implements OnInit {
 
   modalRef: any;
+  isEditing: boolean = false;
+  products: Array<any> = [];
+  loading: any = {
+    creatingOrUpdating: false,
+    getting: false,
+    deleting: false
+  }
   productForm: FormGroup = new FormGroup({
-
+    name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    key: new FormControl('', [Validators.required]),
+    description: new FormControl('', []),
   });
 
   constructor(
-    private modalService: BsModalService
+    private modalService: BsModalService,
+    private http: HttpService,
+    private toast: ToastService,
+    public form: FormService
   ) { }
 
   ngOnInit(): void {
   }
 
   OpenModal(template: any) {
-    this.modalRef = this.modalService.show(template, {backdrop: 'static'});
+    this.modalRef = this.modalService.show(template, {backdrop: 'static', keyboard: false});
   }
 
   CloseModal() {
     if(this.modalRef) this.modalRef.hide();
+  }
+
+  GetProducts() {
+    this.http.Get(`Products`).subscribe((products: any) => {
+      this.products = products;
+    }, err => {
+      console.error("Error al obtener los productos", err);
+    });
+  }
+
+  CreateProduct() {
+    if(!this.productForm.valid) {
+      this.toast.ShowDefaultWarning(`Favor de llenar el formulario`, `Formulario incompleto`);
+      this.productForm.markAllAsTouched();
+      return;
+    }
+
+    this.loading.creatingOrUpdating = true;
+    this.http.Post(`Products`, {product: this.productForm.value}).subscribe((newProduct: any) => {
+      this.toast.ShowDefaultSuccess(`Producto creado correctamente`);
+      this.GetProducts();
+      this.loading.creatingOrUpdating = false;
+    }, err => {
+      this.toast.ShowDefaultDanger(`Error al crear producto`);
+      console.error("Error al crear producto", err);
+      this.loading.creatingOrUpdating = false;
+    });
+  }
+  
+  UpdateProduct() {
+    if(!this.productForm.valid) {
+      this.toast.ShowDefaultWarning(`Favor de llenar el formulario`, `Formulario incompleto`);
+      this.productForm.markAllAsTouched();
+      return;
+    }
+    
+    this.loading.creatingOrUpdating = true;
+    this.http.Patch(`Products`, {product: this.productForm.value}).subscribe((productUpdated: any) => {
+      this.GetProducts();
+      this.loading.creatingOrUpdating = false;
+    }, err => {
+      this.toast.ShowDefaultDanger(`Error al actualizar el producto`);
+      console.error("Error al actualizar producto", err);
+      this.loading.creatingOrUpdating = false;
+    });
   }
 
 }
