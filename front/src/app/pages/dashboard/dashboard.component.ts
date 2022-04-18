8e33ebde2322fadf67b24bc696ee561611d86d8a
Antonio Romano <aromanomacias@gmail.com>
2022-04-17 21:30:47 -0500

    dashboard view 1.0
    showing products with active offer

--- front/src/app/pages/dashboard/dashboard.component.ts
+++ front/src/app/pages/dashboard/dashboard.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpService } from 'src/app/services/http.service';
 import { RoleService } from 'src/app/services/role.service';
+import { ToastService } from 'src/app/services/toast.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -8,11 +10,29 @@ import { RoleService } from 'src/app/services/role.service';
 })
 export class DashboardComponent implements OnInit {
 
+  productsOffered: Array<any> = [];
+  loading: any = {
+    getting: false
+  }
+
   constructor(
-    private role: RoleService
+    public http: HttpService,
+    private toast: ToastService,
   ) { }
 
   ngOnInit(): void {
+    this.GetOfferedProducts();
+  }
+
+  GetOfferedProducts() {
+    this.loading.getting = true;
+    this.http.Get(`/Products/OfferedAsCostumer/1`).subscribe((productsOffered: any) => {
+      this.productsOffered = productsOffered;
+      this.loading.getting = false;
+    }, err => {
+      console.error("Error al obtener las ofertas", err);
+      this.loading.getting = false;
+    });
   }
 
 }
