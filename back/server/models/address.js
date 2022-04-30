64e803f504f342eb8cc311a77937edb35e5bb650
Antonio Romano <aromanomacias@gmail.com>
2022-04-30 13:54:02 -0500

    adding user address

--- back/server/models/address.js
+++ back/server/models/address.js
@@ -2,4 +2,20 @@
 
 module.exports = function(Address) {
 
+    Address.CreateOne = function(address, callback) {
+        Address.create(address, (err, newAddress) => {
+            if(err) return callback(err);
+
+            return callback(null, newAddress);
+        });
+    }
+
+    Address.GetUserAddresses = function(userId, callback) {
+        Address.find({where: {userId}}, (err, userAddresses) => {
+            if(err) return callback(err);
+
+            return callback(null, userAddresses);
+        });
+    }
+
 };
--- front/src/app/components/user-addresses/user-addresses.component.ts
+++ front/src/app/components/user-addresses/user-addresses.component.ts
@@ -3,6 +3,7 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { FormService } from 'src/app/services/form.service';
 import { HttpService } from 'src/app/services/http.service';
 import { ModalService } from 'src/app/services/modal.service';
+import { RoleService } from 'src/app/services/role.service';
 import { ToastService } from 'src/app/services/toast.service';
 
 @Component({
@@ -13,28 +14,44 @@ import { ToastService } from 'src/app/services/toast.service';
 export class UserAddressesComponent implements OnInit {
 
   userAddresses: Array<any> = [];
+  user: any = {};
+  loading: any = {
+    getting: false,
+    creating: false,
+  }
   addressForm: FormGroup = new FormGroup({
-    street: new FormControl('', [Validators.required]),
-    externalNumber: new FormControl('', [Validators.required]),
-    internalNumber: new FormControl('', []),
-    colony: new FormControl('', [Validators.required]),
-    postalCode: new FormControl('', [Validators.required, Validators.pattern(/^[0-9]{1,}$/), Validators.minLength(5), Validators.maxLength(5)]),
-    betweenStreets: new FormControl('', []),
-    references: new FormControl('', [])
+    street: new FormControl(null, [Validators.required]),
+    externalNumber: new FormControl(null, [Validators.required]),
+    internalNumber: new FormControl(null, []),
+    colony: new FormControl(null, [Validators.required]),
+    postalCode: new FormControl(null, [Validators.required, Validators.pattern(/^[0-9]{1,}$/), Validators.minLength(5), Validators.maxLength(5)]),
+    betweenStreets: new FormControl(null, []),
+    references: new FormControl(null, [])
   });
 
   constructor(
     private http: HttpService,
     public modal: ModalService,
     public form: FormService,
-    private toast: ToastService
+    private toast: ToastService,
+    private role: RoleService
   ) { }
 
   ngOnInit(): void {
+    this.GetUserAddresses();
+    this.user = this.role.GetUser();
   }
 
   GetUserAddresses() {
-
+    this.loading.getting = true;
+    const user = this.role.GetUser();
+    this.http.Get(`/Addresses/OfUser/${user ? user.id : 0}`).subscribe((userAddresses: any) => {
+      this.userAddresses = userAddresses;
+      this.loading.getting = false;
+    }, err => {
+      console.error("Error al obtener las direcciones del usuario", err);
+      this.loading.getting = false;
+    });
   }
 
   AddAddressToUser() {
@@ -44,7 +61,29 @@ export class UserAddressesComponent implements OnInit {
       return;
     }
 
+    this.loading.creating = true;
+    const user = this.role.GetUser();
+    let address = {
+      ...this.addressForm.value,
+      userId: user.id
+    }
+    this.http.Post(`/Addresses`, {address}).subscribe(newUserAddress => {
+      this.toast.ShowDefaultSuccess(`Dirección creada`);
+      this.modal.CloseModal();
+      this.form.ResetForm(this.addressForm);
+      this.GetUserAddresses();
+      this.loading.creating = false;
+    }, err => {
+      console.error("Error al crear la dirección", err);
+      this.toast.ShowDefaultDanger(`Error al crear direccón`);
+      this.loading.creating = false;
+    })
+  }
 
+  GetStreetAddress(address: any) {
+    let streetAddress = `${address.street} #${address.externalNumber}`;
+    if(address.internalNumber) streetAddress = streetAddress.concat(` int. ${address.internalNumber}`);
+    return streetAddress
   }
 
 }
