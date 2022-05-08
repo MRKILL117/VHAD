65074617ebe947f336d6d489af4f4bd33e9f81a0
Antonio Romano <aromanomacias@gmail.com>
2022-05-08 13:09:19 -0500

    creating orders correctly

--- back/server/models/order-status.js
+++ back/server/models/order-status.js
@@ -1,5 +1,27 @@
 'use strict';
 
-module.exports = function(Orderstatus) {
+module.exports = function(OrderStatus) {
+
+    OrderStatus.GetAll = function(callback) {
+        OrderStatus.find({}, (err, orderStatuses) => {
+            if(err) return callback(err);
+
+            orderStatuses = orderStatuses.sort((a, b) => {
+                if(a.name > b.name) return 1;
+                if(a.name < b.name) return -1;
+                return 1;
+            });
+            return callback(null, orderStatuses);
+        });
+    }
+
+    OrderStatus.GetByName = function(name, callback) {
+        OrderStatus.findOne({where: {name: {like: `%${name}%`}}}, (err, orderStatus) => {
+            if(err) return callback(err);
+
+            if(!orderStatus) return callback({errorCode: 504, message: 'instance not found'});
+            return callback(null, orderStatus);
+        });
+    }
 
 };
--- back/server/models/order.js
+++ back/server/models/order.js
@@ -5,26 +5,32 @@ const globalVariables = require('../global.js');
 module.exports = function(Order) {
 
     Order.CreateOne = function(cartProducts, address, callback) {
-        const order = {
-            creationDate: moment().tz(globalVariables.momentTimeZone),
-            userId: address.userId,
-            adressId: address.id
-        }
-        Order.create(order, (err, newOrder) => {
+        Order.app.models.OrderStatus.GetByName('pendiente', (err, orderStatus) => {
             if(err) return callback(err);
-            
-            let cont = 0, limit = cartProducts.length;
-            cartProducts.forEach(cartProduct => {
-                const orderProductInstance = {
-                    productId: cartProduct.product.id,
-                    orderId: newOrder.id,
-                    quantity: cartProduct.quantity,
-                }
-                Order.app.models.OrderProduct.create(orderProductInstance, (err, orderProductRelation) => {
-                    if(err) return callback(err);
 
-                    cont++;
-                    if(cont == limit) return callback(null, newOrder);
+            const order = {
+                creationDate: moment().tz(globalVariables.momentTimeZone).toISOString(),
+                userId: address.userId,
+                addressId: address.id,
+                conektaId: null,
+                statusId: orderStatus.id
+            }
+            Order.create(order, (err, newOrder) => {
+                if(err) return callback(err);
+                
+                let cont = 0, limit = cartProducts.length;
+                cartProducts.forEach(cartProduct => {
+                    const orderProductInstance = {
+                        productId: cartProduct.product.id,
+                        orderId: newOrder.id,
+                        quantity: cartProduct.quantity,
+                    }
+                    Order.app.models.OrderProduct.create(orderProductInstance, (err, orderProductRelation) => {
+                        if(err) return callback(err);
+    
+                        cont++;
+                        if(cont == limit) return callback(null, newOrder);
+                    });
                 });
             });
         });
