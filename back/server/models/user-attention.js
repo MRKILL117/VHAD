6d9489bfb407ecfa78ea52536eff7b815dab8127
Antonio Romano <aromanomacias@gmail.com>
2022-05-21 18:42:19 -0500

    sending help request to admins

--- back/server/models/user-attention.js
+++ back/server/models/user-attention.js
@@ -1,4 +1,5 @@
 'use strict';
+const moment = require('moment-timezone');
 
 module.exports = function(UserAttention) {
 
@@ -8,8 +9,49 @@ module.exports = function(UserAttention) {
         userAttention.userId = userId;
         UserAttention.create(userAttention, (err, newAttention) => {
             if(err) return callback(err);
+            
+            newAttention.SendToAdmins((err, sended) => {
+                if(err) return callback(err);
+                
+                return callback(null, newAttention);
+            })
+        });
+    }
 
-            return callback(null, newAttention);
+    UserAttention.prototype.SendToAdmins = function(callback) {
+        UserAttention.findById(this.id, {include: 'user'}, (err, helpRequest) => {
+            if(err) return callback(err);
+
+            UserAttention.app.models.Account.GetAllAccounts((err, users) => {
+                if(err) return callback(err);
+    
+                const adminsEmails = users.filter(user => user.role.name == 'Admin').map(admin => admin.email);
+                const htmlParams = {
+                    userRequestName: helpRequest.user().name,
+                    requestSubject: helpRequest.subject,
+                    request: helpRequest.comment,
+                    platformName: 'VHAD',
+                    currentYear: moment().tz(`America/Mexico_City`).year()
+                }
+                const html = UserAttention.app.models.Mail.GenerateHtml('help-request.ejs', htmlParams);
+                console.log(adminsEmails);
+                let cont = 0, limit = adminsEmails.length;
+                if(!limit) return callback(null, true);
+                adminsEmails.forEach(adminEmail => {
+                    const emailData = {
+                        to: adminEmail,
+                        subject: 'Petición de ayuda VHAD',
+                        text: '',
+                        html
+                    }
+                    UserAttention.app.models.Mail.SendEmail(emailData, (err, mailSent) => {
+                        if(err) return callback(err);
+        
+                        cont++;
+                        if(cont == limit) return callback(null, true);
+                    });
+                });
+            });
         });
     }
 
