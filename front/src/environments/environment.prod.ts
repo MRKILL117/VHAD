26356e4f7ba835122841c5e3e983e6c045d5fac2
Antonio Romano <aromanomacias@gmail.com>
2022-05-31 21:54:06 -0500

    persistance

--- front/src/environments/environment.prod.ts
+++ front/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'http://localhost:3000/api',
+  apiBaseUrl: 'http://162.240.79.39/vhadapi',
   hostBaseUrl: 'http://162.240.79.39/',
   conektaPublicKey: 'key_Iarax8aH5eYB4RGYsyctJyg',
 };
--- front/src/environments/environment.ts
+++ front/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiBaseUrl: 'http://localhost:3000/api',
+  apiBaseUrl: 'http://162.240.79.39/vhadapi',
   hostBaseUrl: 'http://localhost:4200',
   conektaPublicKey: 'key_Iarax8aH5eYB4RGYsyctJyg',
 };
