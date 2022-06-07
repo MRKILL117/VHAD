fca045fe525f0007b8ebc0fe44badb69ed478387
Antonio Romano <aromanomacias@gmail.com>
2022-06-07 00:41:39 -0500

    persistence

--- front/src/environments/environment.ts
+++ front/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   apiBaseUrl: 'http://localhost:3000/api',
-  hostBaseUrl: 'http://localhost:4200',
+  hostBaseUrl: 'http://localhost:4201',
   conektaPublicKey: 'key_Iarax8aH5eYB4RGYsyctJyg',
 };
 
